93fb23954e7a82d9e1dbdfb05caf6865e3ca4d59
michael.yj.liu <michaelliu2210@hotmail.com>
2020-07-21 16:47:40 -0400

    Can now rotate the tetromino. Added an up next tetromino grid. Added start/pause functionality to button. Some bugs to be fixed with start/pause button (e.g. when game is paused you can still move the tetromino using the arrow keys, when the game restarts the up next tetromino changes)

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // We want our JavaScript to talk to all the squares in our grid. Use the querySelectorAll to collect all the divs inside the element of classname, grid
   // Array.from colects all the divs in our grid (200) and turns them into an array we can work with. Now each div will have a specific index number.
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-butto n')
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
   // Width of our grid in squares
   const width = 10
+  let nextRandom = 0
+  let timerId
 
   // The Tetrominoes
   const lTetromino = [
@@ -79,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Make the tetromino move down every second
-  timerID = setInterval(moveDown, 1000)
+  // timerId = setInterval(moveDown, 1000)
 
   // Assign functions to keyCodes. Listen for anytime we press a key on our keyboard
   // we are going to listen out which one was clicked and invoke the control function
@@ -88,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (event.keyCode === 37) {
       moveLeft()
     } else if (event.keyCode === 38) {
-      // rotate()
+      rotate()
     } else if (event.keyCode === 39) {
       moveRight()
     } else if (event.keyCode === 40) {
@@ -97,25 +99,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   document.addEventListener('keyup', control)
 
-  // moveDown function
-  function moveDown () {
-    undraw()
-    currentPosition += width
-    draw()
-    // stop function gets invoked to check every second
-    stop()
-  }
-
   // stop function checks if the tetromino has entered a position that is occupied.
   // If so the tetrmonio stops and the next one begins to fall.
   function stop () {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('occupied'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('occupied'))
       // Start a new Tetromino
-      random = Math.floor(Math.random() * tetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * tetrominoes.length)
       current = tetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayNext()
     }
   }
 
@@ -155,4 +150,62 @@ document.addEventListener('DOMContentLoaded', () => {
     // Finally we redraw the tetromino in it's new position
     draw()
   }
+
+  // moveDown function
+  function moveDown () {
+    undraw()
+    currentPosition += width
+    draw()
+    // stop function gets invoked to check every second
+    stop()
+  }
+
+  // Rotate the tetromino
+  function rotate () {
+    undraw()
+    currentRotation++
+    if (currentRotation === current.length) { // If current rotation is 4, reset back to 0
+      currentRotation = 0
+    }
+    current = tetrominoes[random][currentRotation]
+    draw()
+  }
+
+  // Show the "up next" tetromino in the mini grid
+  const displaySquares = document.querySelectorAll('.mini-grid div') // Note here Array.from is not used like above for variable, squares
+  const displayWidth = 4
+  let displayIndex = 0
+
+  // The tetrominoes without rotations for our mini grid to display
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], // l tetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], // z tetromino
+    [1, displayWidth + 1, displayWidth, displayWidth + 2], // t tetromino
+    [0, 1, displayWidth, displayWidth + 1], // o tetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] // i tetromino
+  ]
+
+  // Function to display the "up next" tetromino in the mini grid
+  function displayNext () {
+    // Remove any trace of a tetrmonio from the mini grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
+
+  // Adding functionality to start/stop button
+  startBtn.addEventListener('click', () => {
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+    } else {
+      draw()
+      timerId = setInterval(moveDown, 1000)
+      nextRandom = Math.floor(Math.random() * tetrominoes.length)
+      displayNext()
+    }
+  })
 })
